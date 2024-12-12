86c760977dfbb81ac11f150a4510fe571654f79a
pimatskku <p.i.m@g.skku.edu>
2024-12-12 13:32:38 +0900

    Update downloader.py
    removed wrong print line

--- downloader.py
+++ downloader.py
@@ -134,4 +134,3 @@ if __name__ == "__main__":
     print("Starting download...")
     results = download_all()
     logger.info("Downloader module run completed.")
-    print(f"Downloading complete: {counts}")
